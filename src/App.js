fb0d01d32107f754222fd406d46408ebf6c6c3b8
Shawon Saha <shawondroid@gmail.com>
2021-07-03 15:49:13 +0600

    Accordion Final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  // console.log(questions);
+
+  return (
+    <main>
+      <div className="container">
+        <h3>Frequently Asked Questions</h3>
+        <section className="info">
+          {questions.map((qs) => {
+            return <SingleQuestion key={qs.id} {...qs} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  // console.log(question);
+
+  const [isHidden, setIsHidden] = useState(true);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setIsHidden(!isHidden)}>
+          {isHidden ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </header>
+      <p>{isHidden ? info.substr(0, 0) : info}</p>
+    </article>
+  );
 };
 
 export default Question;
